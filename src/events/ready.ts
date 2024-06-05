b79ad6271e0dc0850a385f63eb1e8702280724da
Johan <johan.janin@gmail.com>
2024-06-05 12:16:35 +0200

    Bug fixes

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,5 @@
 import type WTBClient from "../main.js";
 import BotEvent from "../utils/event.js";
-import { version } from "../../package.json";
 
 export default class Ready extends BotEvent {
 	constructor(client: WTBClient) {
@@ -8,6 +7,6 @@ export default class Ready extends BotEvent {
 	}
 
 	async onTriggered(...parameters: any[]): Promise<any> {
-		console.log(`${this.client.user?.tag} (version: ${version}) is now ready to use.`);
+		console.log(`${this.client.user?.tag} is now ready to use.`);
 	}
 }
